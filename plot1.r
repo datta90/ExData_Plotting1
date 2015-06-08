738795cdaa517e0fd9db7838940a5f03413c67d2
datta90 <datta.sadhu@yahoo.in>
2015-06-08 20:07:36 +0530

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,4 +1,4 @@
 a2=read.table('1.txt',header=T,sep=';',skip=65000,nrows=5000)
 a1=rbind(a2[a2[1]=='1/2/2007' , ],a2[a2[1]=='2/2/2007' , ])
 #plot the graph
-hist(a1[,3],col='green',main='global active power',xlab='global active power(kilowats')
+hist(a1[,3],col='red',main='global active power',xlab='global active power(kilowats')
